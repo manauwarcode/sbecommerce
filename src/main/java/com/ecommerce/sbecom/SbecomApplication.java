de8171d80f4a4695475ac33a73f68ec86c2dbda6
manauwarcode <manauwar.aws@gmail.com>
2024-09-15 17:44:27 +0530

    project setup commit

---
+++ src/main/java/com/ecommerce/sbecom/SbecomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sbecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/sbecom/SbecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sbecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

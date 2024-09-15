6620e1f272d7b953ac478a77160dacfadc5d1dcc
manauwarcode <manauwar.aws@gmail.com>
2024-09-15 20:37:57 +0530

    added self genrating ids by intself functionality

--- src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -5,20 +5,24 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
     private List<Category> categories = new ArrayList<>();
 
+    private Long uniqueId = 1L;
+
     @Override
     public List<Category> getAllCategories() {
         return categories;
     }
 
+
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(uniqueId++);
         categories.add(category);
-
     }
 }

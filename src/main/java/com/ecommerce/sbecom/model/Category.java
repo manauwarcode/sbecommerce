4a0ab3083c5e75eed92b425f801d3f5b221dd433
manauwarcode <manauwar.aws@gmail.com>
2024-09-16 02:53:42 +0530

    added properties related to database

--- src/main/java/com/ecommerce/sbecom/model/Category.java
+++ src/main/java/com/ecommerce/sbecom/model/Category.java
@@ -1,7 +1,12 @@
 package com.ecommerce.sbecom.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name="categories")
 public class Category {
 
+    @Id
     private long categoryId;
     private String categoryName;
 

0812f613e2be4ea1f19b11e03b1477ef1a39a067
manauwarcode <manauwar.aws@gmail.com>
2024-09-15 18:33:11 +0530

    created category service and acheievd loose coupling

--- src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -1,27 +1,30 @@
 package com.ecommerce.sbecom.controller;
 
 import com.ecommerce.sbecom.model.Category;
+import com.ecommerce.sbecom.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return this.categoryService.getAllCategories();
     }
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
-        return "Category Added Successfully";
+        this.categoryService.createCategory(category);
+        return "Category Added Successfully!";
     }
 }

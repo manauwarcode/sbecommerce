079eb1abd700e4901ba49aef50ee078b1eea1e0f
manauwarcode <manauwar.aws@gmail.com>
2024-09-16 20:45:47 +0530

    added jpa validation dependencies and aaded the validation on the fields

--- src/main/java/com/ecommerce/sbecom/model/Category.java
+++ src/main/java/com/ecommerce/sbecom/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 
@@ -14,6 +15,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long categoryId;
+
+    @NotBlank(message = "Category Name Must Not Be Blank!!")
     private String categoryName;
 
 }

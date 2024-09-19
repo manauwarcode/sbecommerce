02146205ce46acf8a49758b2dd18191bf7e6ac37
manauwarcode <manauwar.aws@gmail.com>
2024-09-19 13:11:21 +0530

    removed unused imports

--- src/main/java/com/ecommerce/sbecom/service/CategoryService.java
+++ src/main/java/com/ecommerce/sbecom/service/CategoryService.java
@@ -4,8 +4,6 @@ import com.ecommerce.sbecom.payload.CategoryDTO;
 import com.ecommerce.sbecom.payload.CategoryResponse;
 
 
-import java.util.List;
-
 public interface CategoryService {
 
     CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize,String sortBy,String sortOrder);
